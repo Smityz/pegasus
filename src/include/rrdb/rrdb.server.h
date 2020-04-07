ceeecf7db2918f074f6e07c42d48b94fd61a96c0
smityz <smityz@qq.com>
2020-04-07 21:37:31 +0800

    format

--- src/include/rrdb/rrdb.server.h
+++ src/include/rrdb/rrdb.server.h
@@ -3,6 +3,7 @@
 #include <dsn/dist/replication/replication_app_base.h>
 #include <dsn/dist/replication/storage_serverlet.h>
 #include <rrdb/rrdb.code.definition.h>
+#include <dsn/cpp/serverlet.h>
 
 namespace dsn {
 namespace apps {
