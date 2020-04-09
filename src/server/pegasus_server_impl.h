74e609a9c4ccd489a06043bae9f36b7b1359d7b4
smityz <smityz@qq.com>
2020-04-10 00:00:49 +0800

    1

--- src/server/pegasus_server_impl.h
+++ src/server/pegasus_server_impl.h
@@ -56,9 +56,12 @@ public:
     virtual void on_scan(const ::dsn::apps::scan_request &args,
                          ::dsn::rpc_replier<::dsn::apps::scan_response> &reply) override;
     virtual void on_clear_scanner(const int64_t &args) override;
-    virtual void on_detect_hotkey(const ::dsn::apps::hotkey_detect_request &args) override;
     virtual void
-    on_stop_detect_hotkey(const ::dsn::apps::stop_hotkey_detect_request &args) override;
+    on_detect_hotkey(const ::dsn::apps::hotkey_detect_request &args,
+                     ::dsn::rpc_replier<::dsn::apps::hotkey_detect_response> &reply) override;
+    virtual void on_stop_detect_hotkey(
+        const ::dsn::apps::stop_hotkey_detect_request &args,
+        ::dsn::rpc_replier<::dsn::apps::stop_hotkey_detect_response> &reply) override;
 
     // input:
     //  - argc = 0 : re-open the db
