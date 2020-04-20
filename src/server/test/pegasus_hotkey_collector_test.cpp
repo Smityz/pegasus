f1e8315004b00ad74b17fb872c6409631faba629
smityz <smityz@qq.com>
2020-04-20 11:00:40 +0800

    kmaxtime

--- src/server/test/pegasus_hotkey_collector_test.cpp
+++ src/server/test/pegasus_hotkey_collector_test.cpp
@@ -57,12 +57,7 @@ TEST(hotkey_detect_test, find_hotkey)
             collector->analyse_data();
         }
     }
-    sleep(20);
-    collector->analyse_data();
-    collector->analyse_data();
-    collector->analyse_data();
-    collector->analyse_data();
-    collector->analyse_data();
+    collector->kMaxTime_sec = 0;
     ASSERT_EQ(collector->get_status(), "STOP");
     ASSERT_EQ(collector->get_result(result), false);
 
