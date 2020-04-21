e556e5c0c5ff662ec94f2053f7e334ddd517d394
smityz <smityz@qq.com>
2020-04-21 15:41:14 +0800

    rpc_holder add capture_data

--- src/server/pegasus_hotkey_collector.cpp
+++ src/server/pegasus_hotkey_collector.cpp
@@ -105,7 +105,7 @@ void hotkey_collector::capture_msg_data(dsn::message_ex **requests, const int co
              }
              */
             if (rpc_code == dsn::apps::RPC_RRDB_RRDB_PUT) {
-                put_rpc::rpc_holder rpc(requests[i]);
+                rpc_holder<dsn::apps::update_request, dsn::apps::update_response> rpc(requests[i]);
                 key = rpc.request().key;
             }
             if (key.length() < 2)
