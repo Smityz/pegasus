94503bdb91d80b3b19ad1a4ba1016f77e3176bb3
smityz <smityz@qq.com>
2020-04-21 10:43:01 +0800

    rpc_holder add capture_data

--- src/server/pegasus_server_impl.cpp
+++ src/server/pegasus_server_impl.cpp
@@ -621,7 +621,7 @@ int pegasus_server_impl::on_batched_write_requests(int64_t decree,
 {
     dassert(_is_open, "");
     dassert(requests != nullptr, "");
-    //_write_hotkey_collector->capture_msg_data(requests, count);
+    _write_hotkey_collector->capture_msg_data(requests, count);
 
     return _server_write->on_batched_write_requests(requests, count, decree, timestamp);
 }
