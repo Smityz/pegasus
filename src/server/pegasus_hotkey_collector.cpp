b4f34a8c4b986fb8098522bc9d834866b69f77f4
smityz <smityz@qq.com>
2020-04-21 14:23:17 +0800

    rpc_holder add capture_data

--- src/server/pegasus_hotkey_collector.cpp
+++ src/server/pegasus_hotkey_collector.cpp
@@ -76,22 +76,19 @@ bool hotkey_collector::analyse_fine_data()
     return true;
 }
 
-void hotkey_collector::capture_msg_data(dsn::message_ex **requests_raw, const int count)
+void hotkey_collector::capture_msg_data(dsn::message_ex **requests, const int count)
 {
     if (_collector_state.load(std::memory_order_seq_cst) == STOP || count == 0) {
         return;
     }
-
-    dsn::message_ex requests;
-    memcpy(&requests, requests_raw, sizeof(*requests_raw));
-
     for (int i = 0; i < count; i++) {
         ::dsn::blob key;
         if (requests[i] != nullptr && requests[i]->buffers.size() >= 2) {
             dsn::task_code rpc_code(requests[0]->rpc_code());
             if (rpc_code == dsn::apps::RPC_RRDB_RRDB_MULTI_PUT) {
-                auto rpc = multi_put_rpc::capture_data(requests[0]);
-                key = rpc.request().hash_key;
+                std::unique_ptr<multi_put_request> thrift_request;
+                unmarshall(requests[0], *thrift_request);
+                key = thrift_request.hash_key;
             }
             if (rpc_code == dsn::apps::RPC_RRDB_RRDB_INCR) {
                 auto rpc = incr_rpc::capture_data(requests[0]);
