d636fb9afd861f295d270e797ae5ff407ffb150c
smityz <smityz@qq.com>
2020-05-25 11:52:19 +0800

    test

--- src/server/fine_hotkey_collector.cpp
+++ src/server/fine_hotkey_collector.cpp
@@ -85,11 +85,11 @@ bool hotkey_fine_data_collector::analyse_fine_data(std::string &result)
             count_max_key = iter.first;
         }
     }
-    derror("=============");
+    derror("<<<<<<<<<<");
     for (auto iter : data_samples) {
         derror("%d ", iter);
     }
-    derror("=============");
+    derror(">>>>>>>>>>");
     if (hotkey_collector::variance_cal(data_samples, hot_values, _data_variance_threshold)) {
         result = count_max_key;
         return true;
