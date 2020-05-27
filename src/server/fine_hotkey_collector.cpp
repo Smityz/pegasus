91dfcc132fe90f93246f518d02094090be76df10
smityz <smityz@qq.com>
2020-05-27 11:06:41 +0800

    test

--- src/server/fine_hotkey_collector.cpp
+++ src/server/fine_hotkey_collector.cpp
@@ -51,7 +51,7 @@ inline int hotkey_fine_data_collector::get_queue_index()
 
 void hotkey_fine_data_collector::capture_fine_data(const std::string &data)
 {
-    std::cout << data << std::endl;
+    derror("samekey:%s", data);
     if (_hotbucket_hash(data, _hotkey_collector_data_fragmentation) != _target_bucket)
         return;
     rw_queues[get_queue_index()].try_emplace(data);
